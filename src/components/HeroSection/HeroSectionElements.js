937bae8887480825ce6c2baea8b7e07c87ff96b2
magus <harvestashya@gmail.com>
2022-05-17 18:16:00 -0700

    hero styling

--- src/components/HeroSection/HeroSectionElements.js
+++ src/components/HeroSection/HeroSectionElements.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const HeroContainer = styled.div`
-  background: #0c0c0c;
+  background: #38474e;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0 30px;
-  height: 800px;
+  height: 95vh;
   position: relative;
   z-index: 1;
 `
@@ -17,11 +16,24 @@ export const HeroBg = styled.div`
   right: 0;
   bottom: 0;
   left: 0;
+  background-image: url(${props => props.image});
+  background-color: #38474e;
+  background-position: center;
+  background-size: cover;
   width: 100%;
   height: 100%;
   overflow: hidden;
 `
 
-export const VideoBg = styled.video`
-  
+export const HeroText = styled.div`
+  position: absolute;
+  font-size: 6rem;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  color: white;
+  -webkit-text-stroke: 1.5px;
+  -webkit-text-stroke-color: black;
 `
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {
   HeroContainer,
-  HeroBg
-} from './HeroSectionElements'
+  HeroBg,
+  HeroText
+} from './HeroSectionElements';
+import bg from '../../images/background.jpg'; //credit: Stephen Leonardi from unsplash
 
 export default function HeroSection() {
   return (
     <HeroContainer>
-      <HeroBg>
-        test
+      <HeroBg image={bg}>
+        <HeroText>
+          test
+        </HeroText>
       </HeroBg>
     </HeroContainer>
   )
