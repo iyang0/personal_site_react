c47621fb58e5f05d7134d0c28012f3d7606af45a
magus <harvestashya@gmail.com>
2022-05-26 01:21:31 -0700

    gradiant effect to hero section

--- src/components/HeroSection/HeroSectionElements.js
+++ src/components/HeroSection/HeroSectionElements.js
@@ -9,6 +9,21 @@ export const HeroContainer = styled.div`
   height: 95vh;
   position: relative;
   z-index: 1;
+
+  //gradiant effect to transition into content.
+  :before{
+    content:'';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(
+      180deg, 
+      rgba(0, 0, 0, 0) 80%,
+      rgba(56, 	71, 78, .3) 100%);
+    z-index: 2;
+  }
 `
 
 export const HeroBg = styled.div`
