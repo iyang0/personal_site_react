5d1892757e7ae007dd0ae95bf0a81c8bdfd72c7e
magus <harvestashya@gmail.com>
2022-06-23 21:07:36 -0700

    add images and styling to project list

--- src/components/ListSection/ListSectionElements.js
+++ src/components/ListSection/ListSectionElements.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { themeCloud, themeOffBlack } from "../../consts/consts";
+import { themeCloud, themeGrey, themeOffBlack } from "../../consts/consts";
 
 export const ListContainer = styled.div`
   height: 100%;
@@ -33,6 +33,7 @@ export const ItemsWrapper = styled.div`
 `;
 
 export const ItemCard = styled.div`
+  position: relative;
   background: ${themeCloud};
   display: flex;
   flex-direction: column;
@@ -40,7 +41,6 @@ export const ItemCard = styled.div`
   align-items: center;
   border-radius: 10px;
   max-height: 340px;
-  padding: 30px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
   transition: all 0.02s ease-in-out;
 
@@ -51,10 +51,67 @@ export const ItemCard = styled.div`
   }
 `;
 
+export const LinkLeft = styled.a`
+  text-decoration: none;
+  color: white;
+  font-size: 2rem;
+  -webkit-text-stroke: 1px;
+  -webkit-text-stroke-color: black;
+  position: absolute;
+  height: 100%;
+  width: 50%;
+  left: 0;
+`;
+
+export const LeftLinkWrap = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${themeGrey};
+  border-radius: 10px 0 0 10px;
+  z-index: 10;
+  opacity: 0;
+  transition: all .3s ease-in-out;
+
+  &:hover{
+    opacity: 80%;
+  }
+`;
+
+export const LinkRight = styled.a`
+  text-decoration: none;
+  color: white;
+  font-size: 2rem;
+  -webkit-text-stroke: 1px;
+  -webkit-text-stroke-color: black;
+  position: absolute;
+  height: 100%;
+  width: 50%;
+  right: 0;
+`;
+
+export const RightLinkWrap = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${themeGrey};
+  border-radius: 0 10px 10px 0;
+  z-index: 10;
+  opacity: 0;
+  transition: all .3s ease-in-out;
+
+  &:hover{
+    opacity: 80%;
+  }
+`;
+
 export const ItemImage = styled.img`
-  height: 160px;
-  width: 160px;
+  height: 100%;
+  width: 100%;
   margin-bottom: 10px;
+  border-radius: 10px 10px 0 0;
 `;
 
 export const ListTitle = styled.h1`
@@ -65,14 +122,14 @@ export const ListTitle = styled.h1`
   @media screen and (max-width: 480px) {
     font-size: 2rem;
   }
-`
+`;
 
 export const ItemTitle = styled.h2`
   font-size: 1rem;
   margin-bottom: 10px;
-`
+`;
 
 export const ItemDescription = styled.p`
   font-size: 1rem;
   text-align: center;
-`
+`;
--- src/components/ListSection/index.js
+++ src/components/ListSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ListContainer,
   ListTitle,
@@ -6,8 +6,16 @@ import {
   ItemCard,
   ItemImage,
   ItemTitle,
-  ItemDescription
-} from './ListSectionElements';
+  ItemDescription,
+  LinkLeft,
+  LeftLinkWrap,
+  RightLinkWrap,
+  LinkRight,
+} from "./ListSectionElements";
+import personal from "../../images/project1.png";
+import joblyBack from "../../images/project2.png";
+import joblyFront from "../../images/project3.png";
+import warbler from "../../images/project4.png";
 
 export default function ListSection() {
   return (
@@ -15,52 +23,63 @@ export default function ListSection() {
       <ListTitle>Projects</ListTitle>
       <ItemsWrapper>
         <ItemCard>
-          <a href="https://ivan-yang.xyz/">deployed</a>
-          <a href="https://github.com/iyang0/personal_site_react">github</a>
-          <ItemImage />
-          <ItemTitle>
-            This Personal Site
-          </ItemTitle>
+          <LinkLeft href="https://ivan-yang.xyz/">
+            <LeftLinkWrap>deployed</LeftLinkWrap>
+          </LinkLeft>
+          <LinkRight href="https://github.com/iyang0/personal_site_react">
+            <RightLinkWrap>github</RightLinkWrap>
+          </LinkRight>
+          <ItemImage src={personal} />
+          <ItemTitle>This Personal Site</ItemTitle>
           <ItemDescription>
-            A wholely react project to mess around with styled components
+            A wholely react project to mess around with styled components.
           </ItemDescription>
         </ItemCard>
         <ItemCard>
-          <a href="https://jobly-backend-ivan.herokuapp.com/companies">deployed</a>
-          <a href="https://github.com/iyang0/jobly-backend">github</a>
-          <ItemImage />
-          <ItemTitle>
-            Jobly Backend
-          </ItemTitle>
+          <LinkLeft href="https://jobly-backend-ivan.herokuapp.com/companies">
+            <LeftLinkWrap>deployed</LeftLinkWrap>
+          </LinkLeft>
+          <LinkRight href="https://github.com/iyang0/jobly-backend">
+            <RightLinkWrap>github</RightLinkWrap>
+          </LinkRight>
+          <ItemImage src={joblyBack} />
+          <ItemTitle>Jobly Backend</ItemTitle>
           <ItemDescription>
-            The backend to an API to a mock jobs listing site made using JS/express and a postgres db.
-            The main points of interest here is authentication and authorization using JWTs.
+            The backend to an API to a mock jobs listing site made using
+            JS/express. The main point of interest is
+            authentication and authorization using JWTs.
           </ItemDescription>
         </ItemCard>
         <ItemCard>
-          <a href="https://jobly-ivan.surge.sh/">deployed</a>
-          <a href="https://github.com/iyang0/jobly-backend">github</a>
-          <ItemImage />
-          <ItemTitle>
-            Jobly Frontend
-          </ItemTitle>
+          <LinkLeft href="https://jobly-ivan.surge.sh/">
+            <LeftLinkWrap>deployed</LeftLinkWrap>
+          </LinkLeft>
+          <LinkRight href="https://github.com/iyang0/jobly-backend">
+            <RightLinkWrap>github</RightLinkWrap>
+          </LinkRight>
+          <ItemImage src={joblyFront} />
+          <ItemTitle>Jobly Frontend</ItemTitle>
           <ItemDescription>
-            The frontend for the API backend. Uses react and bootstrap. 
-            Use testuser/password for username/password if you want to try it out.
+            The frontend for the API backend. Uses react and bootstrap. Use
+            testuser/password for username/password if you want to try it out.
           </ItemDescription>
         </ItemCard>
         <ItemCard>
-          <a href="https://warbler-ivan-lance.herokuapp.com/">deployed</a>
-          <a href="https://github.com/iyang0/warbler">github</a>
-          <ItemImage />
-          <ItemTitle>
-            Warbler
-          </ItemTitle>
+          <LinkLeft href="https://warbler-ivan-lance.herokuapp.com/">
+            <LeftLinkWrap>deployed</LeftLinkWrap>
+          </LinkLeft>
+          <LinkRight href="https://github.com/iyang0/warbler">
+            <RightLinkWrap>github</RightLinkWrap>
+          </LinkRight>
+          <ItemImage src={warbler} />
+          <ItemTitle>Warbler</ItemTitle>
           <ItemDescription>
-            A small clone of twitter made with Python/Flask.
+            A small clone of twitter made with Python/Flask. Use
+            tuckerdiane/passowrd for username/password if you want to use a test
+            account.
           </ItemDescription>
         </ItemCard>
       </ItemsWrapper>
     </ListContainer>
-  )
+  );
 }
