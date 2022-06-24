0140c8ddd1e6960f443dd4e6a7d05414620e143c
magus <harvestashya@gmail.com>
2022-06-24 00:42:04 -0700

    change title and one of the links

--- src/components/ContentSection/Data.js
+++ src/components/ContentSection/Data.js
@@ -1,21 +1,35 @@
-import profile from '../../images/profile.jpg';
-import hobbies from '../../images/hobbies.jpg';
-import { ExternalLink } from './ContentElements';
+import profile from "../../images/profile.jpg";
+import hobbies from "../../images/hobbies.jpg";
+import { ExternalLink } from "./ContentElements";
 
 export const aboutMeContent = {
   id: "about",
   lightTheme: false,
   topLine: "Hi, I'm Ivan Yang",
   headLine: "Fullstack Developer",
-  description: <>I am a native San Franciscan born and raised, graduated from SFSU in Economics and have turned to Software Engineering;
-   I've had an interesting path taking me from underwriting for credit card processors to developing software. 
-   I have experience working with HTML, CSS, JavaScript(Express, Node, React), Python(Flask), and PostgreSQL.
-   <br/> <br/>
-   Currently, I am working as a backend engineer at <ExternalLink href='https://www.joinuplift.co/'>UpLift</ExternalLink>.</>,
+  description: (
+    <>
+      I am a native San Franciscan born and raised, graduated from SFSU in
+      Economics and have turned to Software Engineering; I've had an interesting
+      path taking me from underwriting for credit card processors to developing
+      software. I have experience working with HTML, CSS, JavaScript(Express,
+      Node, React), Python(Flask), and PostgreSQL.
+      <br /> <br />
+      Currently, I am working as a backend engineer at{" "}
+      <ExternalLink
+        href="https://www.joinuplift.co/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        UpLift
+      </ExternalLink>
+      .
+    </>
+  ),
   imgStart: false,
   img: profile,
   alt: "It's me",
-  lightNext: true
+  lightNext: true,
 };
 
 export const interestsContent = {
@@ -30,5 +44,5 @@ export const interestsContent = {
   imgStart: true,
   img: hobbies,
   alt: "My hobbies: manga, mtg, and games",
-  lightNext: false
+  lightNext: false,
 };
